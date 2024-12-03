962ccf1fe181e26acb66ddf21decc476d49c9a8e
pirana <21ug0272@sltc.ac.lk>
2024-12-03 23:34:50 +0530

    Your commit message here

---
+++ src/components/Product.js
@@ -0,0 +1,37 @@
+import {useState} from 'react';
+import '../assets/CSS/layout.css';
+export default function Product({key,product,setCart,setTot}){
+    const[quantity,setQuantity]=useState(0);
+    
+    const addToCart = () =>{
+        if (quantity > 0){
+            setCart((prev)=>[
+                ...prev,
+                {
+                    product: product.name,
+                    quantity,
+                    price: product.price * quantity,
+                },
+            ]);
+            setTot((prevTotal)=> prevTotal + product.price * quantity);
+        }
+    };
+    return(
+        <div className="grid-item" key={key}>
+
+            <div class="card">
+                <img  
+               src={require(`../assets/image/${product.img}`)} alt={product.name} />
+                <div class="card-body">
+                    <h5 class="card-title">{product.name}Price:</h5>
+                    <div class="quantity-container">
+                        <label for="quantity">Quantity:</label>
+                        <input type="number" id="quantity" name="quantity" value={quantity}
+                        onChange={(e) => { setQuantity(e.target.value); }}/>
+                    </div>
+                    <button class="card-button" onClick={addToCart}>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Products.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import '../assets/CSS/layout.css';
+import { flowers } from "./FlowerDB";
+import Product from "./Product";
+import Cart from "./Cart";
+
+export default function Products(){
+    const[cart,setCart]=useState([]);
+    const[total,setTot]=useState(0);
+   
+    return(
+        <>
+            <div className="item1">
+                <h1>Flower Shop</h1>
+            </div>
+            <div className="item2">
+                <h4 className="card-title">Buy flowers</h4>
+                <div className="grid-container">
+                {flowers.map((flower, index) => {
+                    return (
+                    <Product key={index} product={flower} setCart={setCart} setTot={setTot} />
+            );
+          })}
+                </div>
+
+            </div>
+            <div className="item3">
+                {
+                <Cart cart={cart} total={total}/>
+                }
+            </div>
+        </>
+    );
+
+}
